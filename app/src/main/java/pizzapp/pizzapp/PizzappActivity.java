381f9851c29d33087b3217c960ca7aebc1097f3d
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2018-03-09 19:10:32 +0100

    WIP

--- app/src/main/java/pizzapp/pizzapp/PizzappActivity.java
+++ app/src/main/java/pizzapp/pizzapp/PizzappActivity.java
@@ -2,8 +2,12 @@ package pizzapp.pizzapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,8 +27,35 @@ public class PizzappActivity extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, pizzaList);
-        Spinner pizzaSpinner = (Spinner) findViewById(R.id.pizzaChoiceSpinner);
+        final Spinner pizzaSpinner = findViewById(R.id.pizzaChoiceSpinner);
         pizzaSpinner.setAdapter(adapter);
 
+        final TextView odooUserName = findViewById(R.id.odooUserName);
+
+        Button orderButton = findViewById(R.id.orderButton);
+
+        final ImageView illustrationNiaise = findViewById(R.id.illustrationNiaise);
+
+        // Hide the image when the user is entering its odoo username
+        odooUserName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                illustrationNiaise.setVisibility(View.GONE);
+                TextView tv = (TextView) v;
+                if (tv.getText().toString().equals("Odoo user")) {
+                    tv.setText("");
+                }
+            }
+        });
+
+        orderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                illustrationNiaise.setVisibility(View.VISIBLE);
+                odooUserName.setEnabled(false);
+                pizzaSpinner.setEnabled(false);
+            }
+        });
     }
+
 }
